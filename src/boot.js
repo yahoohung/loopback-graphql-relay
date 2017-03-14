16e6c0d580fd43969d4387d616d4fac8048cdbb4
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-03-14 18:53:50 +0500

    first compile, no resolvers

--- src/boot.js
+++ src/boot.js
@@ -4,22 +4,31 @@ const graphql = require('graphql-server-express');
 const gqlTools = require('graphql-tools');
 const bodyParser = require('body-parser');
 // const _ = require('lodash');
+const { GraphQLSchema, GraphQLObjectType } = require('graphql');
 
 const ast = require('./ast');
+const viewerAst = require('./viewerAst');
 const resolvers = require('./resolvers');
 const typeDefs = require('./typedefs');
 
 module.exports = function(app, options) {
   const models = app.models();
   const types = ast(models);
+  types.Viewer = viewerAst(models);
   const typeDefObjs = typeDefs(types);
 
-  const schema = gqlTools.makeExecutableSchema({
-    typeDefs: typeDefObjs,
-    resolvers: resolvers(models),
-    resolverValidationOptions: {
-      requireResolversForAllFields: false
-    }
+  const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'Query',
+      fields: {
+        // node: nodeField,
+        viewer: {
+          type: typeDefObjs.Viewer,
+          args: {},
+          resolve: (root, args, context) => ({ id: 'foo' })
+        }
+      }
+    })
   });
 
   const graphiqlPath = options.graphiqlPath || '/graphiql';
---
+++ src/viewerAst.js
@@ -0,0 +1,38 @@
+const _ = require('lodash');
+const execution = require('./execution');
+const { connectionArgs } = require('graphql-relay');
+
+
+// building types for Viewer
+module.exports = function abstractTypes(models) {
+
+  const Viewer = {
+    category: 'TYPE',
+    name: 'Viewer',
+    description: 'Viewer',
+		// interfaces: () => [nodeDefinitions.nodeInterface],
+    fields: {}
+  };
+
+  _.forEach(models, (model) => {
+
+    if (!model.shared) {
+      return;
+    }
+
+    Viewer.fields[_.lowerFirst(model.pluralModelName)] = {
+      // relation: true,
+      // root: true,
+      // name: model.modelName,
+      args: connectionArgs,
+			// gqlType: utils.connectionTypeName(model),
+      gqlType: model.modelName,
+      list: true,
+      resolver: (obj, args, context) => {
+        execution.findAll(model, obj, args, context);
+      }
+    };
+  });
+
+  return Viewer;
+};

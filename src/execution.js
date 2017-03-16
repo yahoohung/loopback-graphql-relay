ed3381314b01416d8e53ce57eb95e8f1a42ab4f6
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-03-16 13:36:03 +0500

    bug fixes

--- src/execution.js
+++ src/execution.js
@@ -1,130 +1,102 @@
 'use strict';
+
 const _ = require('lodash');
 const utils = require('./utils');
 
 function buildSelector(model, args) {
-    let selector = {
-        where: args.where || {}
-    };
-    const begin = utils.getId(args.after);
-    const end = utils.getId(args.before);
+  const selector = {
+    where: args.where || {}
+  };
+  const begin = utils.getId(args.after);
+  const end = utils.getId(args.before);
 
-    selector.skip = args.first - args.last || 0;
-    selector.limit = args.last || args.first;
+  selector.skip = args.first - args.last || 0;
+  selector.limit = args.last || args.first;
 
-    if (model.getIdName && model.getIdName()){
-        selector.order = model.getIdName() + (end ? ' DESC' : ' ASC');
-        if (begin) {
-            selector.where[model.getIdName()] = selector[model.getIdName()] || {};
-            selector.where[model.getIdName()].gt = begin;
-        }
-        if (end) {
-            selector.where[model.getIdName()] = selector[model.getIdName()] || {};
-            selector.where[model.getIdName()].lt = end;
-        }
+  if (model.getIdName && model.getIdName()) {
+    selector.order = model.getIdName() + (end ? ' DESC' : ' ASC');
+    if (begin) {
+      selector.where[model.getIdName()] = selector[model.getIdName()] || {};
+      selector.where[model.getIdName()].gt = begin;
     }
-    return selector;
+    if (end) {
+      selector.where[model.getIdName()] = selector[model.getIdName()] || {};
+      selector.where[model.getIdName()].lt = end;
+    }
+  }
+  return selector;
 }
 
 function findOne(model, obj, args, context) {
-    let id = obj ? obj[model.getIdName()] : args.id;
-    return model.findById(id);
+  const id = obj ? obj[model.getIdName()] : args.id;
+  return model.findById(id);
 }
 
 function getCount(model, obj, args, context) {
-    return model.count(args.where);
+  return model.count(args.where);
 }
 
 function getFirst(model, obj, args) {
-    return model.findOne({
-            order: model.getIdName() + (args.before ? ' DESC' : ' ASC'),
-            where: args.where
-        })
-        .then(res => {
-            return res ? res.__data : {};
-        });
+  return model.findOne({
+    order: model.getIdName() + (args.before ? ' DESC' : ' ASC'),
+    where: args.where
+  })
+        .then(res => res ? res.__data : {});
 }
 
 function getList(model, obj, args) {
-    return model.find(buildSelector(model, args));
+  return model.find(buildSelector(model, args));
 }
 
 function findAll(model, obj, args, context) {
-    return getList(model, obj, args);
-        // .then(data => {
-        //     return Promise.resolve(_.map(data, o => o.toJSON()));
-        // });
-    const response = {
-        args: args
-    };
-    return getCount(model, obj, args)
-        .then(count => {
-            response.count = count;
-            return getFirst(model, obj, args);
-        })
-        .then(first => {
-            response.first = first;
-            return getList(model, obj, args);
-        })
-        .then(list => {
-            response.list = list;
-            return response;
-        });
+  return getList(model, obj, args);
 }
 
 function findRelated(rel, obj, args, context) {
-    if (_.isArray(obj[rel.keyFrom])) {
-        return [];
-    }
-    args.where = {
-        [rel.keyTo]: obj[rel.keyFrom]
-    };
-    return findAll(rel.modelTo, obj, args, context);
+  if (_.isArray(obj[rel.keyFrom])) {
+    return Promise.resolve([]);
+  }
+  args.where = {
+    [rel.keyTo]: obj[rel.keyFrom]
+  };
+  return findAll(rel.modelTo, obj, args, context);
 
 }
 
 function resolveConnection(model, obj, args, context) {
-    return {
-        [utils.connectionTypeName(model)]: {
-            totalCount: (obj, args, context) => {
-                return obj.count;
-            },
+  return {
+    [utils.connectionTypeName(model)]: {
+      totalCount: (obj, args, context) => obj.count,
 
-            edges: (obj, args, context) => {
-                return _.map(obj.list, node => {
-                    return {
-                        cursor: utils.idToCursor(node[model.getIdName()]),
-                        node: node
-                    };
-                });
-            },
+      edges: (obj, args, context) => _.map(obj.list, node => ({
+        cursor: utils.idToCursor(node[model.getIdName()]),
+        node
+      })),
 
-            [model.pluralModelName]: (obj, args, context) => {
-                return obj.list;
-            },
+      [model.pluralModelName]: (obj, args, context) => obj.list,
 
-            pageInfo: (obj, args, context) => {
-                let pageInfo = {
-                    startCursor: null,
-                    endCursor: null,
-                    hasPreviousPage: false,
-                    hasNextPage: false
-                };
-                if (obj.count > 0) {
-                    pageInfo.startCursor = utils.idToCursor(obj.list[0][model.getIdName()]);
-                    pageInfo.endCursor = utils.idToCursor(obj.list[obj.list.length - 1][model.getIdName()]);
-                    pageInfo.hasNextPage = obj.list.length === obj.args.limit;
-                    pageInfo.hasPreviousPage = obj.list[0][model.getIdName()] !== obj.first[model.getIdName()].toString();
-                }
-                return pageInfo;
-            }
+      pageInfo: (obj, args, context) => {
+        const pageInfo = {
+          startCursor: null,
+          endCursor: null,
+          hasPreviousPage: false,
+          hasNextPage: false
+        };
+        if (obj.count > 0) {
+          pageInfo.startCursor = utils.idToCursor(obj.list[0][model.getIdName()]);
+          pageInfo.endCursor = utils.idToCursor(obj.list[obj.list.length - 1][model.getIdName()]);
+          pageInfo.hasNextPage = obj.list.length === obj.args.limit;
+          pageInfo.hasPreviousPage = obj.list[0][model.getIdName()] !== obj.first[model.getIdName()].toString();
         }
-    };
+        return pageInfo;
+      }
+    }
+  };
 }
 
 module.exports = {
-    findAll,
-    findOne,
-    findRelated,
-    resolveConnection
+  findAll,
+  findOne,
+  findRelated,
+  resolveConnection
 };

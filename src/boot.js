8adc8e07ea4a180296566ca8eb0145b841366745
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-03-21 00:02:22 +0500

    more code cleaning

--- src/boot.js
+++ src/boot.js
@@ -4,12 +4,8 @@ const graphql = require('graphql-server-express');
 const bodyParser = require('body-parser');
 const { getSchema } = require('./schema/index');
 
-const getTypes = require('./types');
-
 module.exports = function(app, options) {
   const models = app.models();
-
-  getTypes(models);
   const schema = getSchema(models);
 
   const graphiqlPath = options.graphiqlPath || '/graphiql';
--- src/schema/index.js
+++ src/schema/index.js
@@ -3,8 +3,12 @@
 const { GraphQLSchema } = require('graphql');
 const getQuery = require('./query');
 const getMutation = require('./mutation');
+const getTypes = require('../types');
 
 function getSchema(models) {
+
+  getTypes(models);
+
   return new GraphQLSchema({
     query: getQuery(models),
     mutation: getMutation(models),

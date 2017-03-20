0fff73ead9022c9d7262dfc2e6dabfff3fe90792
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-03-20 23:56:19 +0500

    code clean up

--- src/boot.js
+++ src/boot.js
@@ -1,51 +1,16 @@
 'use strict';
 
 const graphql = require('graphql-server-express');
-const gqlTools = require('graphql-tools');
 const bodyParser = require('body-parser');
-// const _ = require('lodash');
-const { GraphQLSchema, GraphQLObjectType } = require('graphql');
-
 const { getSchema } = require('./schema/index');
-
-
-const ast = require('./ast');
-const viewerAst = require('./viewerAst');
-const resolvers = require('./resolvers');
-const typeDefs = require('./typedefs');
-
 
 const getTypes = require('./types');
 
 module.exports = function(app, options) {
   const models = app.models();
 
-
-  const queryDefObjs = getTypes(models);
-
+  getTypes(models);
   const schema = getSchema(models);
-
-  // const types = ast(models);
-  // types.Viewer = viewerAst(models);
-  // types.node = {
-  //   gqlType: 'node'
-  // };
-
-  // const typeDefObjs = typeDefs(types, models);
-
-  // const schema = new GraphQLSchema({
-  //   query: new GraphQLObjectType({
-  //     name: 'Query',
-  //     fields: {
-  //       node: typeDefObjs.node,
-  //       viewer: {
-  //         type: typeDefObjs.Viewer,
-  //         args: {},
-  //         resolve: (root, args, context) => ({ id: 'foo' })
-  //       }
-  //     }
-  //   })
-  // });
 
   const graphiqlPath = options.graphiqlPath || '/graphiql';
   const path = options.path || '/graphql';

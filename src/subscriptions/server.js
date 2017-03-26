4037424e0a8073a37b77924ca241c7dc52ed4c05
Abdul Rehman Talat <rehman.talat@gmail.com>
2017-03-26 14:03:19 +0500

    added placeholder to login using accessToken

--- src/subscriptions/server.js
+++ src/subscriptions/server.js
@@ -27,10 +27,12 @@ module.exports = function(subscriptionManager, opts) {
 
   const server = new SubscriptionServer(
       Object.assign({}, {
-        // onConnect: ({ accessToken }) => {
-        //   // Implement if you need to handle and manage connection
-        // TODO: Implement authentication, reference blog: https://dev-blog.apollodata.com/new-release-of-graphql-subscriptions-for-javascript-f11be19e6569#.l1yh2y1x1
-        // },
+        onConnect: ({ accessToken }) => {
+          // Implement if you need to handle and manage connection
+          // TODO: Implement authentication, reference blog: https://dev-blog.apollodata.com/new-release-of-graphql-subscriptions-for-javascript-f11be19e6569#.l1yh2y1x1
+          // var ctx = req.loopbackContext;
+          // if (ctx && ctx.active) ctx.set('accessToken', token);
+        },
         subscriptionManager
       }, options),
       Object.assign({}, {

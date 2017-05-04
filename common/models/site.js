6c9eb5dd3e9b7bca96edb1b9864da8afbd8099ca
amnajunaid <amna.junaid@blueeast.com>
2017-05-04 20:30:34 +0500

    adding sample data

---
+++ common/models/site.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(Site) {
+
+/*
+  Site.sites = function(httpRequest, httpResponse, cb) {
+
+    var Account = Site.app.models.Account;
+
+    const userId =   httpRequest.accessToken.userId.toString();
+
+    Account.findById(userId, {include: 'sites'}, function(error, data) {
+
+      return cb(null, data.__data.sites);
+
+    });
+
+  };
+
+  Site.remoteMethod('sites', {
+    accepts: [
+      {arg: 'req', type: 'object', 'http': {source: 'req'}},
+      {arg: 'res', type: 'object', 'http': {source: 'res'}}
+    ],
+    returns: {arg: '', type: "array", root:true},
+    http: {path: '/', verb: 'get'}
+  });
+*/
+};
--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,10 +1,5 @@
-// Copyright IBM Corp. 2015. All Rights Reserved.
-// Node module: loopback-example-relations
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
 'use strict';
 
 module.exports = function enableAuthentication(server) {
-  // enable authentication
+	server.enableAuth({ datasource: 'db' });
 };
